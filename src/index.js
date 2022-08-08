c87dea5d6d44b0f6b2b45ccdca36b63aa6268088
EstiTw <estitwitowork@gmail.com>
2022-08-08 12:32:18 +0300

    added files project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Counters from "./Components/Counters";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Counters />);
