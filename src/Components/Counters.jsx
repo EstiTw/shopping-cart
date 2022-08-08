f98151c2942e088c7c4aa5593cffd61b032ab98f
EstiTw <estitwitowork@gmail.com>
2022-08-08 14:57:14 +0300

    adding delete, increment and reset buttons

--- src/Components/Counters.jsx
+++ src/Components/Counters.jsx
@@ -9,16 +9,46 @@ class Counters extends Component {
       { id: 3, value: 2 },
       { id: 4, value: 0 },
     ],
+    reset: false,
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((counter) => {
+      counter.value = 0;
+      return counter;
+    });
+    this.setState({ counters });
   };
 
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset Cart
+        </button>
+
         {this.state.counters.map((counter) => (
           <Counter
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
             key={counter.id}
-            value={counter.value}
-            id={counter.id}
+            counter={counter}
           ></Counter>
         ))}
       </div>
